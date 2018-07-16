acdaf59e51f47e3d5e542e3139bff56944d308cd
phamthanh <phphamvanthanh@gmail.com>
2018-07-16 11:22:00 +0000

    init jwt

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ var db = require('./db');
 var UserController = require('./user/UserController');
 app.use('/users', UserController);
 
+// app.js
+var AuthController = require('./auth/AuthController');
+app.use('/api/auth', AuthController);
+module.exports = app;
+
 module.exports = app;
--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+mongoose.connect('mongodb://admin:admin123@ds139331.mlab.com:39331/jwt');
